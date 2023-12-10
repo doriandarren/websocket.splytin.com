46f7446eb501b9047b0e10ed09ab4de14cf6932f
Dorian Gonzalez <doriandarren1@gmail.com>
2023-12-10 12:31:08 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,31 @@
+## Websocket
+
+
+### Script
+
+```sh
+# Creamos las carpetas src y subcarpetas
+mkdir -p src/{assets,js,css}
+
+# Creamos los ficheros HTML, CSS y Javascript
+touch src/index.html
+touch src/js/index.js
+touch src/css/index.css
+
+
+
+npm install ws
+
+
+npm install -g nodemon
+
+
+npm install express ws
+
+
+node src/main.js
+
+
+```
+
+
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ 
+  server
+});
+
+
+// // Crear un servidor HTTP
+// const server = http.createServer((req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Servidor HTTP en ejecución');
+// });
+
+
+
+// Manejar conexiones WebSocket
+wss.on('connection', (ws) => {
+  console.log('Cliente conectado al WebSocket');
+
+  // Manejar mensajes del cliente
+  ws.on('message', (message) => {
+    console.log(`Mensaje recibido: ${message}`);
+
+    // Enviar un mensaje de vuelta al cliente
+    ws.send(`Servidor: Recibí tu mensaje - ${message}`);
+
+    // Enviar el mensaje a todos los clientes excepto al remitente
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(`Usuario: ${message}`);
+      }
+    });
+
+  });
+
+  // Manejar cierre de conexión
+  ws.on('close', () => {
+    console.log('Cliente desconectado del WebSocket');
+  });
+});
+
+// Ruta de prueba para mostrar un HTML con cliente WebSocket
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
